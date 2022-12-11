05f0eb45ea3774802e2b096ccd2a77a6c768398d
Prashik Kamble <kambleprashik7349@gmail.com>
2022-12-11 16:26:38 +0530

    MODIFIED : Code to validate the third mandatory part.

--- src/regexforpincodeandemail/Main.java
+++ src/regexforpincodeandemail/Main.java
@@ -14,6 +14,6 @@ public class Main {
     }
 
     public void creatingRegexPatternForEmail() {
-        System.out.println("\nValidating the second mandatory part : " + Pattern.matches("^[a-z]{3}[@][a-z]{3,}$", "abc@bridgelabz"));
+        System.out.println("\nValidating the third mandatory part : " + Pattern.matches("^[a-z]{3}[@][a-z]{3,}[.][co]{2}$", "abc@bridgelabz.co"));
     }
 }
