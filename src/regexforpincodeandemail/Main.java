dd1becba11a36d2e4db44d7c46843f4cd6d8cb1d
Prashik Kamble <kambleprashik7349@gmail.com>
2022-12-11 15:47:20 +0530

    ADDED : Code for creating a regex pattern to accept 6-digit number.

--- src/regexforpincodeandemail/Main.java
+++ src/regexforpincodeandemail/Main.java
@@ -1,7 +1,14 @@
 package regexforpincodeandemail;
 
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Java Regex!");
+        new Main().creatingRegexPattern();
+    }
+
+    public void creatingRegexPattern() {
+        System.out.println("\nSix digit number for Pin code : " + Pattern.matches("[0-9]{6}", "400088"));
     }
 }
