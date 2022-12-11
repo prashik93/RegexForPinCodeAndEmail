0b76bba0c4d0e96c066ee6864ea55253b986ae2a
Prashik Kamble <kambleprashik7349@gmail.com>
2022-12-11 16:13:12 +0530

    ADDED : Code to validate first mandatory part of email.

--- src/regexforpincodeandemail/Main.java
+++ src/regexforpincodeandemail/Main.java
@@ -5,10 +5,15 @@ import java.util.regex.Pattern;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Java Regex!");
-        new Main().creatingRegexPattern();
+        new Main().creatingRegexPatternForPinCode();
+        new Main().creatingRegexPatternForEmail();
     }
 
-    public void creatingRegexPattern() {
+    public void creatingRegexPatternForPinCode() {
         System.out.println("\nSix digit number for Pin code : " + Pattern.matches("^(([0-9]{6})|([0-9]{3}[\s][0-9]{3}))$", "400 088"));
+    }
+
+    public void creatingRegexPatternForEmail() {
+        System.out.println("\nValidating the first mandatory part : " + Pattern.matches("^[a-z]{3}", "abc"));
     }
 }
