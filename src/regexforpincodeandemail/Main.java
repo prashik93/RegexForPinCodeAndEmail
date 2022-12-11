66716ea13ad9c638471dc35f91fe000e3b121580
Prashik Kamble <kambleprashik7349@gmail.com>
2022-12-11 16:32:09 +0530

    MODIFIED : Code to validate special characters.

--- src/regexforpincodeandemail/Main.java
+++ src/regexforpincodeandemail/Main.java
@@ -14,6 +14,6 @@ public class Main {
     }
 
     public void creatingRegexPatternForEmail() {
-        System.out.println("\nValidating the third mandatory part : " + Pattern.matches("^[a-z]{3}[@][a-z]{3,}[.][co]{2}$", "abc@bridgelabz.co"));
+        System.out.println("\nValidating the third mandatory part : " + Pattern.matches("^[a-z]{3}[.|_|+|-][a-z]{3,}[@][a-z]{3,}[.][co]{2}$", "abc.xyz@bridgelabz.co"));
     }
 }
